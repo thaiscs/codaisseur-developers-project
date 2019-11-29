975386bd9010411503a14ed62628521a491d8d52
thaiscs <thais.advj@gmail.com>
2019-11-29 16:10:01 +0100

    added NavLink

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 import "./App.css";
 import DevelopersList from "./components/DevelopersList";
 import Developer from "./components/Developer";
-import { Route } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import PostPage from "./components/PostPage";
 import LoginPage from "./components/LoginPage";
-import { Link } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import AddPost from "./components/AddPost";
 import Profile from "./components/Profile";
@@ -16,23 +15,31 @@ function App() {
       <ul className="navbar">
         <li>
           {" "}
-          <Link to="/">Home</Link>
+          <NavLink to="/" activeStyle={{ color: "green" }}>
+            Home
+          </NavLink>
         </li>
         <li>
-          <Link to="/login">Login</Link>
+          <NavLink to="/login" activeStyle={{ color: "green" }}>
+            Login
+          </NavLink>
         </li>
         <li>
-          <Link to="/signup">Sign Up</Link>
+          <NavLink to="/signup" activeStyle={{ color: "green" }}>
+            Sign Up
+          </NavLink>
         </li>
         <li>
-          <Link to="/posts/new">New Post</Link>
+          <NavLink to="/posts/new" activeStyle={{ color: "green" }}>
+            New Post
+          </NavLink>
         </li>
       </ul>
 
-      <Route path="/login" component={LoginPage} />
-      <Route path="/signup" component={SignUp} />
-      <Route path="/posts/new" component={AddPost} />
-      <Route path="/read/:id" component={PostPage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/posts/new" component={AddPost} />
+      <Route exact path="/read/:id" component={PostPage} />
       <Route exact path="/" component={DevelopersList} />
       <Route exact path="/developers" component={Developer} />
       <div>
